feaf860bee9a9c6ae4c568712521bcae51cff628
sumitskj <sumitjangirdss.1@gmail.com>
2018-10-21 02:44:14 +0530

    deploy heroku

---
+++ server.js
@@ -0,0 +1,22 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/SpotifyApp'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/SpotifyApp/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
